444de814e74d76162169628c9d23417260c8131b
Ricardo Silva <ricardo@loadingplay.com>
2016-09-26 15:22:37 -0300

    fix bsale

--- fixemitteddocs/fixdocs.py
+++ fixemitteddocs/fixdocs.py
@@ -3,7 +3,7 @@
 
 
 import bsale
-# from lp.json_util import loads
+from lp.json_util import loads
 
 
 if __name__ == "__main__":
@@ -11,65 +11,76 @@ if __name__ == "__main__":
     file = open("emitteddocs.json")
     file_content = file.read().strip()
 
-    # json_data = loads(file_content)
-    json_data = { 
-        "items" : [
-            # {'id': 1747},
-            # {'id': 1748},
-            # {'id': 1749},
-            # {'id': 1750},
-            # {'id': 1751},
-            # {'id': 1752},
-            # {'id': 1753},
-            # {'id': 1754},
-            # {'id': 1755},
-            # {'id': 1756},
-            { 'id' : 1757}
-          ]}
+    json_data = loads(file_content)
+    # json_data = { 
+    #     "items" : [
+    #         # {'id': 1747},
+    #         # {'id': 1748},
+    #         # {'id': 1749},
+    #         # {'id': 1750},
+    #         # {'id': 1751},
+    #         # {'id': 1752},
+    #         # {'id': 1753},
+    #         # {'id': 1754},
+    #         # {'id': 1755},
+    #         # {'id': 1756},
+    #         # { 'id' : 1757}
+    #       ]}
+
+    # last 1796
 
     document = bsale.Document()
 
+    counter = 0
+
     for item in json_data["items"]:
 
-        detail = document.GetDetailDocument(item["id"])
-        details = []
-        for items in detail["items"]:
-            det = {
-                "documentDetailId": items["id"],
-                "quantity": int(items["quantity"]),
-                "unitValue": (items['netAmount']/1.19)
+        if counter < 50 and item["id"] > 1757:
+
+            detail = document.GetDetailDocument(item["id"])
+            details = []
+            for items in detail["items"]:
+                det = {
+                    "documentDetailId": items["id"],
+                    "quantity": int(items["quantity"]),
+                    "unitValue": (items['netAmount']/1.19)
+                }
+                details.append(det)
+
+            # client = {
+            #     "code": "1-9",
+            #     "city": "Puerto Varas",
+            #     "municipality": "comuna",
+            #     "activity": "giro",
+            #     "address": "direccion"
+            #   }
+
+            client = {
+                "municipality": "Las Condes",
+                "activity": "Sin dato",
+                "city": "Santiago",
+                "address": "Alonso de Cordova",
+                "code": "22222222-2"
             }
-            details.append(det)
 
-        # client = {
-        #     "code": "1-9",
-        #     "city": "Puerto Varas",
-        #     "municipality": "comuna",
-        #     "activity": "giro",
-        #     "address": "direccion"
-        #   }
+            params = {
+              "documentTypeId": 2,
+              "officeId": 1,
+              "referenceDocumentId": item["id"],
+              "expirationDate": 1474502400,
+              "emissionDate": 1474502400,
+              "motive": "prueba api",
+              "declareSii": 1,
+              "priceAdjustment": 0,
+              "editTexts": 0,
+              "type": 0,
+              "details":details,
+              "client":client
+            }
 
-        client = {
-            "municipality": "Las Condes",
-            "activity": "Sin dato",
-            "city": "Santiago",
-            "address": "Alonso de Cordova",
-            "code": "22222222-2"
-        }
+            print document.CreateCreditNote(params)
 
-        params = {
-          "documentTypeId": 2,
-          "officeId": 1,
-          "referenceDocumentId": item["id"],
-          "expirationDate": 1474502400,
-          "emissionDate": 1474502400,
-          "motive": "prueba api",
-          "declareSii": 1,
-          "priceAdjustment": 0,
-          "editTexts": 0,
-          "type": 0,
-          "details":details,
-          "client":client
-        }
+        if counter >= 50:
+            break
 
-        print document.CreateCreditNote(params)
+        counter += 1
